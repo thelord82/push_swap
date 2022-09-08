619af1814774e0090eddcf89c9e6b8586eb1bc21
thelord82 <mandrelord@gmail.com>
2022-09-08 16:47:34 -0400

    Updates with function headers

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: malord <malord@student.42quebec.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 15:07:10 by malord            #+#    #+#             */
-/*   Updated: 2022/08/25 15:09:23 by malord           ###   ########.fr       */
+/*   Updated: 2022/09/08 10:38:07 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,22 @@
 
 typedef struct s_stacklist {
 	int					nb;
+	int					index;
+	int					pos;
+	int					target_pos;
+	int					cost_a;
+	int					cost_b;
+	char				**quoted_args;
 	struct s_stacklist	*next;
 }		t_stack;
+
+void	check_split(char **argv, t_stack *stack_a);
+void	to_int_list(char **array, int position, t_stack *stack_a);
+void	check_sorted(t_stack *stack_a, int size);
+void	check_errors(char **argv, int index);
+void	check_numbers(char **argv, int index);
+void	check_doubles(char **argv, int index);
+void	check_limits(char **argv, int index);
+int		lst_size(t_stack *stack_a);
+
 #endif
