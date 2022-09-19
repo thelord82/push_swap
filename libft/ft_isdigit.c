482b94934d1586410e98fa81666cd49c98dcf345
thelord82 <mandrelord@gmail.com>
2022-09-19 11:49:28 -0400

    modified comment

--- libft/ft_isdigit.c
+++ libft/ft_isdigit.c
@@ -6,13 +6,13 @@
 /*   By: malord <malord@student.42quebec.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 08:40:04 by malord            #+#    #+#             */
-/*   Updated: 2022/04/19 08:33:33 by malord           ###   ########.fr       */
+/*   Updated: 2022/09/19 11:38:10 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// Checks if n parameter is a digit
+// Checks if n parameter is a digit in ASCII.
 int	ft_isdigit(int n)
 {
 	if (n >= '0' && n <= '9')
